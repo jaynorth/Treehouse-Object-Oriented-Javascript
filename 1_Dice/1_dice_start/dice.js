d3648f04ee2179007e1bcf301571abe95499b830
jayweb <jayrdee@gmail.com>
2015-09-30 10:32:59 +0200

    updated dice file

--- 1_Dice/1_dice_start/dice.js
+++ 1_Dice/1_dice_start/dice.js
@@ -1,11 +1,13 @@
 
 
 var dice = {
-
+	sides: 6,
 	roll: function () {
-	  var sides = 6;
-	  var randomNumber = Math.floor(Math.random() * sides) + 1;
-	  console.log(randomNumber);
+	  
+	  var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+	  return randomNumber;
 	}
 
 }
+
+
